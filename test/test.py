8578b4fe6388edee1da1aa6dfd354990648cc7a2
yongju-Shin <a786542@gmail.com>
2023-03-21 13:45:11 +0900

    version2

--- test/test.py
+++ test/test.py
@@ -5,9 +5,9 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from util.detection import Detection
 
-model_path = 'C:/Users/user/cp1_yolo/models/best.pt' 
-input_path = 'C:/Users/user/cp1_yolo/data/sample1.mp4' 
-output_path = 'C:/Users/user/cp1_yolo/results/sample1_result.mp4'   
+model_path = '../models/best.pt' 
+input_path = '../data/sample1.mp4' 
+output_path = '../results/sample1_result.mp4'   
 threshold = 0.2
 
 detector = Detection(model_path,threshold)
